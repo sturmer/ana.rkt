9d8739a8b20e4bcb48b4c2c733df3a3304980633
sturmer <galiziacentrale@gergel.im>
2015-10-06 08:25:34 +0200

    Separate tests in module, add README

---
+++ README.md
@@ -0,0 +1,12 @@
+Find anagrams
+=============
+
+One of the intro projects shown at
+[Intro-Projects](https://github.com/racket/racket/wiki/Intro-Projects).
+
+
+Test
+====
+
+Run ``raco test ana-test.rkt``, or open the ana-test.rkt in DrRacket and
+``Ctrl-R`` it.
--- ana.rkt
+++ ana.rkt
@@ -1,7 +1,11 @@
 #lang racket
 (require rackunit rackunit/text-ui)
-(require racket/string)
+
 (require 2htdp/batch-io)
+
+(provide find-anagrams
+         sortw
+         hash-words)
 
 ;(require readline) ; for prompting users
 
@@ -55,62 +59,3 @@ explicitly closed, allowing the query of several words.
 
 ; Alias
 (define find-anagrams fa)
-
-;;;;;;;;;;;;;;;;;
-#| Test suites |#
-;;;;;;;;;;;;;;;;;
-(define sortw-tests
-  (test-suite
-   "sortw suite"
-   
-   (test-equal? "Empty string"              
-                (sortw "") "")
-   
-   (test-equal? "leg -> egl"              
-                (sortw "leg") "egl")
-   (test-case 
-    "Long word with repeated letters"
-    (check-equal? (sortw "heautontimoroumenos") "aeehimmnnoooorsttuu")
-    (check-equal? 
-     (string-length "heautontimoroumenos")
-     (string-length "aeehimmnnoooorsttuu")))   
-   ))
-
-(define anagram-tests
-  (test-suite
-   "anagram suite"
-   
-   (test-case 
-    "Test list hasher"
-    (let* ([expected-hash (hash "egl"  '("gel" "leg")
-                                "aign"  '("gian" "naig" "gnia"))]
-           [result-hash (hash-words '("gel" "leg" "gnia" "gian" "naig"))]
-           [exp-values (hash-ref expected-hash "egl")]
-           [res-values (hash-ref result-hash "egl")])
-      (check-equal? (sort exp-values string<?) 
-                    (sort res-values string<?))))
-   
-   #|
-   (test-case
-    "Read lines"
-    (let* ([file (open-input-file "10-lines.txt")]
-           [exp-list '("A" "A's" "AA's" "AB's" "ABM's" "AC's" 
-                           "ACTH's" "AI's" "AIDS's" "AM's")]           
-           [retrieved-list (read-next-line-iter file)])
-      (check-equal? retrieved-list exp-list))
-    )
-   |#
-   
-   (test-case
-    "Test anagram finder"
-    (let ([an (find-anagrams "live")]
-          [an2 (find-anagrams "revenue")]
-          [an3 (find-anagrams "ixxa")])
-      (check-equal? an '("vile" "veil" "live" "evil"))
-      (check-equal? an2 '("revenue"))
-      (check-equal? an3 (void)) ; Also prints "Not found: ixxa"
-      ))))
-
-
-(run-tests sortw-tests)
-(run-tests anagram-tests)
